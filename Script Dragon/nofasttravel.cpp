657812c0c27e19f8c2c8d43c9bd6df191370b6f6
NullCascade <NullCascade@gmail.com>
2011-11-30 14:01:05 -0800

    Added an ini option that lets the user set how often the check is made, just in case there is some lag by doing it frequently.

--- Script Dragon/nofasttravel.cpp
+++ Script Dragon/nofasttravel.cpp
@@ -1,21 +1,28 @@
-/* 
-	USE THIS ONE AS A TEMPLATE FOR YOUR SCRIPT PLUGINS
-*/
-
+// Script Dragon includes.
 #include "common\skyscript.h"
 #include "common\obscript.h"
 #include "common\types.h"
 #include "common\enums.h"
 #include "common\plugin.h"
 
+
+// Definitions.
+#define CONFIG_FILE "nofasttravel.ini"
+
 void
 main(
 	)
 {
+	// Get the customized delay.
+	int heartbeat = IniReadInt( CONFIG_FILE, "settings", "heartbeat", 500 );
+
+	// Initial disable.
 	Game::EnableFastTravel( false );
+	
+	// Redisable every heartbeat miliseconds.
 	while ( true ) {
+		Wait(heartbeat);
 		if ( Game::IsFastTravelEnabled() ) Game::EnableFastTravel( false );
-		Wait(8);
 		Wait(0);
 	}
 }
