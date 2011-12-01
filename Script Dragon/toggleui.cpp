7c5bd624620af54357f059d538117173a5cc69ba
NullCascade <NullCascade@gmail.com>
2011-12-01 12:22:33 -0800

    Switched to SafeGetKeyPressed.

--- Script Dragon/toggleui.cpp
+++ Script Dragon/toggleui.cpp
@@ -10,6 +10,23 @@
 #define CONFIG_FILE "toggleui.ini"
 
 
+// SafeGetKeyPressed - Checks if the key is pressed, but won't work if in menu mode.
+bool
+SafeGetKeyPressed(
+	BYTE i_Key
+	)
+{
+	// Don't enable in menu mode.
+	if ( Utility::IsInMenuMode() ) return false;
+
+	// Don't enable if the key is set to 0x00.
+	if ( i_Key == 0x00 ) return false;
+
+	// Otherwise, check as normal.
+	return GetKeyPressed( i_Key );
+}
+
+
 // Plugin entry point.
 void
 main(
@@ -21,7 +38,7 @@ main(
 	// Main plugin loop.
 	while ( true ) {
 		// Trigger on bound toggle key.
-		if ( GetKeyPressed( keyToggleUI ) ) {
+		if ( SafeGetKeyPressed( keyToggleUI ) ) {
 			// Toggle menus/UI, then wait half a second.
 			Debug::ToggleMenus();
 			Wait( 500 );
