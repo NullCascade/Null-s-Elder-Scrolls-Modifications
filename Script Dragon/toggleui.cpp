dbd63ad414a38319bf553b9151cf4051ccbeec36
NullCascade <NullCascade@gmail.com>
2011-12-01 17:46:34 -0800

    Added more UI toggle functionality.

--- Script Dragon/toggleui.cpp
+++ Script Dragon/toggleui.cpp
@@ -27,21 +27,54 @@ SafeGetKeyPressed(
 	return GetKeyPressed( i_Key );
 }
 
-// GetMenuIsEnabled - Checks to see if menu drawing is enabled.
-bool
-GetMenuIsEnabled(
+
+// SetHUDOpacity - Sets the hud opacity.
+void
+SetHUDOpacity(
+	float i_Value
 	)
 {
-	return ( *(unsigned char*)( ADDR_DRAW_MENUS ) != 0x00 );
+	Utility::SetINIFloat( "fHUDOpacity", i_Value );
 }
 
-// SetMenusEnabled - Sets menu drawing.
+
+// SetShowUI - Enables/disables entire UI drawing.
 void
-SetMenusEnabled(
-	unsigned char i_Value
+SetShowUI(
+	unsigned int i_Value
 	)
 {
 	*(unsigned char*)( ADDR_DRAW_MENUS ) = i_Value;
+}
+
+
+// SetShowCompass - Enable/disables the rendering of the compass.
+void
+SetShowCompass(
+	bool i_Value
+	)
+{
+	Utility::SetINIBool( "bShowCompass:interface", i_Value );
+}
+
+
+// SetShowQuestMarkers - Enable/disables the rendering of compass(/map?) quest markers.
+void
+SetShowQuestMarkers(
+	bool i_Value
+	)
+{
+	Utility::SetINIBool( "bShowQuestMarkers:gameplay", i_Value );
+}
+
+
+// SetShowFloatingQuestMarkers - Enable/disables the rendering of floating quest markers.
+void
+SetShowFloatingQuestMarkers(
+	bool i_Value
+	)
+{
+	Utility::SetINIBool( "bShowFloatingQuestMarkers:gameplay", i_Value );
 }
 
 
@@ -51,20 +84,59 @@ main(
 	)
 {
 	//  Read initialization file.
-	BYTE keyToggleUI = IniReadInt( CONFIG_FILE, "general", "toggleKey", 0x00 );
+	bool showUI = ( IniReadInt( CONFIG_FILE, "defaults", "show_ui", 1 ) != 0 );
+	bool showCompass = ( IniReadInt( CONFIG_FILE, "defaults", "show_compass", 1 ) != 0 );
+	bool showQuestMarkers = ( IniReadInt( CONFIG_FILE, "defaults", "show_quest_markers", 1 ) != 0 );
+	bool showFloatingQuestMarkers = ( IniReadInt( CONFIG_FILE, "defaults", "show_float_quest_markers", 1 ) != 0 );
+	BYTE keyToggleUI = IniReadInt( CONFIG_FILE, "keys", "toggle_ui", 0x00 );
+	BYTE keyToggleCompass = IniReadInt( CONFIG_FILE, "keys", "toggle_compass", 0x00 );
+	BYTE keyToggleQuestMarkers = ( IniReadInt( CONFIG_FILE, "defaults", "show_quest_markers", 1 ) != 0 );
+	BYTE keyToggleFloatingQuestMarkers = IniReadInt( CONFIG_FILE, "keys", "toggle_float_quest_markers", 0x00 );
 
 	// Keep track of the menu state.
-	bool menusEnabled = true;
-	SetMenusEnabled( menusEnabled );
+	SetShowCompass( showCompass );
+	SetShowQuestMarkers( showQuestMarkers );
+	SetShowFloatingQuestMarkers( showFloatingQuestMarkers );
+	SetShowUI( showUI );
+
+	// Handle multiple keys at once.
+	bool keyPressWait = false;
 	
 	// Main plugin loop.
 	while ( true ) {
-		// Trigger on bound toggle key.
+		// Wait if we just got a key event.
+		if ( keyPressWait ) {
+			Wait( 250 );
+			keyPressWait = false;
+		}
+
+		// Enable/disable UI.
 		if ( SafeGetKeyPressed( keyToggleUI ) ) {
 			// Toggle menus/UI, then wait half a second.
-			menusEnabled = !menusEnabled;
-			SetMenusEnabled( menusEnabled );
-			Wait( 250 );
+			showUI = !showUI;
+			SetShowUI( showUI );
+			keyPressWait = true;
+		}
+
+		// Enable/disable compass.
+		if ( SafeGetKeyPressed( keyToggleCompass ) ) {
+			showCompass = !showCompass;
+			SetShowCompass( showCompass );
+			keyPressWait = true;
+		}
+
+		// Enable/disable quest markers.
+		if ( SafeGetKeyPressed( keyToggleQuestMarkers ) ) {
+			showQuestMarkers = !showQuestMarkers;
+			SetShowFloatingQuestMarkers( showQuestMarkers );
+			keyPressWait = true;
+		}
+
+		// Enable/disable floating quest markers.
+		if ( SafeGetKeyPressed( keyToggleFloatingQuestMarkers ) ) {
+			showFloatingQuestMarkers = !showFloatingQuestMarkers;
+			SetShowFloatingQuestMarkers( showFloatingQuestMarkers );
+			keyPressWait = true;
 		}
 		
 		// Must be called at the end of execution cycle.
