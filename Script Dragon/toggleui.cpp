e5d4ee0c0e012042c0147b00ace2534a6197ed14
NullCascade <NullCascade@gmail.com>
2011-12-01 16:43:56 -0800

    Gets/sets the menu as enabled/disabled manually, instead of using ToggleMenus().

--- Script Dragon/toggleui.cpp
+++ Script Dragon/toggleui.cpp
@@ -8,6 +8,7 @@
 
 // Definitions.
 #define CONFIG_FILE "toggleui.ini"
+#define ADDR_DRAW_MENUS 0x0156D500
 
 
 // SafeGetKeyPressed - Checks if the key is pressed, but won't work if in menu mode.
@@ -26,6 +27,23 @@ SafeGetKeyPressed(
 	return GetKeyPressed( i_Key );
 }
 
+// GetMenuIsEnabled - Checks to see if menu drawing is enabled.
+bool
+GetMenuIsEnabled(
+	)
+{
+	return ( *(unsigned char*)( ADDR_DRAW_MENUS ) != 0x00 );
+}
+
+// SetMenusEnabled - Sets menu drawing.
+void
+SetMenusEnabled(
+	unsigned char i_Value
+	)
+{
+	*(unsigned char*)( ADDR_DRAW_MENUS ) = i_Value;
+}
+
 
 // Plugin entry point.
 void
@@ -33,28 +51,20 @@ main(
 	)
 {
 	//  Read initialization file.
-	BYTE keyToggleUI = IniReadInt( CONFIG_FILE, "general", "toggleKey", 0xDD );
-	BYTE keyScreenshot = IniReadInt( CONFIG_FILE, "screenshot", "screenshot_key", 0x00 );
-	bool turnOffForScreenshots = ( IniReadInt( CONFIG_FILE, "screenshot", "auto_disable", 0 ) ==  1 );
+	BYTE keyToggleUI = IniReadInt( CONFIG_FILE, "general", "toggleKey", 0x00 );
 
 	// Keep track of the menu state.
-	bool menusDisabled = false;
+	bool menusEnabled = true;
+	SetMenusEnabled( menusEnabled );
 	
 	// Main plugin loop.
 	while ( true ) {
 		// Trigger on bound toggle key.
 		if ( SafeGetKeyPressed( keyToggleUI ) ) {
 			// Toggle menus/UI, then wait half a second.
-			Debug::ToggleMenus();
-			menusDisabled = !menusDisabled;
-			Wait( 500 );
-		}
-
-		// Taking screenshot?
-		if ( turnOffForScreenshots && !menusDisabled && SafeGetKeyPressed( keyScreenshot ) ) {
-			Debug::ToggleMenus();
-			Wait( 1000 );
-			Debug::ToggleMenus();
+			menusEnabled = !menusEnabled;
+			SetMenusEnabled( menusEnabled );
+			Wait( 250 );
 		}
 		
 		// Must be called at the end of execution cycle.
